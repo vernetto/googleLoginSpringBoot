6363cc26310c7221b3ddfdfb42910d4925aed9a8
vernetto <Ernesto0z>
2023-12-28 20:08:40 +0100

    first commit

---
+++ src/main/java/com/example/googleloginspringboot/GoogleLoginSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleloginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleLoginSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/googleloginspringboot/WebController.java
@@ -0,0 +1,21 @@
+package com.example.googleloginspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+}
