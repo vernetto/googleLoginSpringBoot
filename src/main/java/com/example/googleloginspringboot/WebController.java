ae22b901655d33560ddf2affdc5f1c69b1c3b873
vernetto <Ernesto0z>
2023-12-28 20:15:47 +0100

    first commit 3

--- src/main/java/com/example/googleloginspringboot/WebController.java
+++ src/main/java/com/example/googleloginspringboot/WebController.java
@@ -1,15 +1,25 @@
 package com.example.googleloginspringboot;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import java.security.Principal;
+
 @Controller
 public class WebController {
 
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, OAuth2AuthenticationToken authentication) {
+        if (authentication != null) {
+            OAuth2User oAuth2User = authentication.getPrincipal();
+            model.addAttribute("user", oAuth2User.getAttributes());
+        }
         return "index";
     }
 
@@ -19,3 +29,4 @@ public class WebController {
     }
 
 }
+
