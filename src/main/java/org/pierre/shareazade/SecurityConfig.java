6ac9d0ecc4d33f20577265796d9e81a81033c26a
vernetto <Ernesto0z>
2024-01-04 21:18:20 +0100

    actuator

--- src/main/java/org/pierre/shareazade/SecurityConfig.java
+++ src/main/java/org/pierre/shareazade/SecurityConfig.java
@@ -5,20 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@EnableWebSecurity(debug = false)
-public class SecurityConfig {
-   //@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+@EnableWebSecurity(debug = true)
+
+
+
+public class SecurityConfig  {
+    //@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        MvcRequestMatcher allPathsMatcher = new MvcRequestMatcher(new HandlerMappingIntrospector(), "/**");
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(allPathsMatcher)
+                        .authenticated()
                 )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")).disable());;
+        return http.build();
+    }
+
+    public SecurityFilterChain filterChainBAD(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**", "/actuator/**")).
+                        permitAll().
+                        anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions().disable())
 
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")));
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")).disable());;
         return http.build();
     }
 
